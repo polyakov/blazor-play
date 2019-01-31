36d7c910662ac83b3cd2177541972b05222c138e
Polyakov <v.polyakov@techsmith.com>
2019-01-31 15:15:05 -0500

    static counter

---
+++ WebApplication1/MyCounter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+   public static class MyCounter
+   {
+      public static int Value;
+   }
+}
